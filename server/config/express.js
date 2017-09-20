9b3a29b8c1c0be64899de72a32a4381cea43a87f
Oscar Miniet <ominiet96@hotmail.com>
2017-09-20 16:32:41 -0400

    turnin

--- server/config/express.js
+++ server/config/express.js
@@ -9,7 +9,7 @@ var path = require('path'),
 
 module.exports.init = function() {
   //connect to database
-  mongoose.connect(config.db.uri);
+  mongoose.connect(config.db.uri, {useMongoClient: true});
 
   //initialize app
   var app = express();
@@ -26,12 +26,17 @@ module.exports.init = function() {
   });
 
   /* serve static files */
+  app.use('/', express.static(__dirname + '/../../client'));
+  app.use('/public', express.static(__dirname + '/../../public'));
   
 
   /* use the listings router for requests to the api */
-
+  app.use('/api/listings', listingsRouter);
 
   /* go to homepage for all routes not specified */ 
+  app.all('/*', function(req, res) {
+    res.sendFile(path.resolve('client/index.html'));
+  });
 
   return app;
 };  
